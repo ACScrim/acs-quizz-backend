4eec2603a01fa66780c0719f95fe62fe07656625
Aubrian Duhayon <aubriduhay@gmail.com>
2025-05-05 12:25:47 +0200

    :sparkles: #19 #20 #21 #22 #23 #24 #25 || Création des endpoints Lobby

--- src/lobbies/lobbies.controller.ts
+++ src/lobbies/lobbies.controller.ts
@@ -1,38 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { LobbiesService } from './lobbies.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/auth.decorator';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { User } from 'src/users/entities/user.entity';
 import { CreateLobbyDto } from './dto/create-lobby.dto';
 import { UpdateLobbyDto } from './dto/update-lobby.dto';
-import { GetUser } from 'src/auth/auth.decorator';
-import { User } from 'src/users/entities/user.entity';
-import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { LobbiesService } from './lobbies.service';
+import { JoinLobbyDto } from './dto/join-lobby.dto';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('lobbies')
 export class LobbiesController {
   constructor(private readonly lobbiesService: LobbiesService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   create(@Body() createLobbyDto: CreateLobbyDto, @GetUser() user: User) {
     return this.lobbiesService.create(createLobbyDto, user);
   }
 
+  @Post('join')
+  join(@GetUser() user: User, @Body() joinLobbyDto: JoinLobbyDto) {
+    return this.lobbiesService.join(user, joinLobbyDto);
+  }
+
+  @Post('leave/:id')
+  leave(@GetUser() user: User, @Param('id') id: string) {
+    return this.lobbiesService.leave(user, id);
+  }
+
   @Get()
   findAll() {
-    return this.lobbiesService.findAll();
+    return this.lobbiesService.findAllPublic();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.lobbiesService.findOne(+id);
+    return this.lobbiesService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateLobbyDto: UpdateLobbyDto) {
-    return this.lobbiesService.update(+id, updateLobbyDto);
+    return this.lobbiesService.update(id, updateLobbyDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.lobbiesService.remove(+id);
+    return this.lobbiesService.remove(id);
   }
 }
--- src/lobbies/lobbies.service.ts
+++ src/lobbies/lobbies.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Lobby } from './entities/lobby.entity';
 import { Model } from 'mongoose';
 import { User } from 'src/users/entities/user.entity';
+import { JoinLobbyDto } from './dto/join-lobby.dto';
 
 @Injectable()
 export class LobbiesService {
@@ -30,20 +31,42 @@ export class LobbiesService {
     return newLobby.save();
   }
 
-  findAll() {
-    return `This action returns all lobbies`;
+  findAllPublic() {
+    return this.lobbyModel
+      .find({ isPublic: true })
+      .populate('owner')
+      .exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} lobby`;
+  async findOne(id: string) {
+    if (!id) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    const lobby = await this.findLobbyById(id);
+    return (await lobby.populate('owner')).populate('players');
   }
 
-  update(id: number, updateLobbyDto: UpdateLobbyDto) {
-    return `This action updates a #${id} lobby`;
+  async update(id: string, updateLobbyDto: UpdateLobbyDto) {
+    if (!id) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    const lobby = await this.findLobbyById(id);
+    if (!lobby) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    return lobby.updateOne(updateLobbyDto, { new: true })
+      .then(() => this.findLobbyById(id));
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} lobby`;
+  async remove(id: string) {
+    if (!id) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    const lobby = await this.findLobbyById(id);
+    if (!lobby) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    return lobby.deleteOne();
   }
 
   async checkPlayersInLobby(lobby: Lobby) {
@@ -53,5 +76,48 @@ export class LobbiesService {
         throw new NotFoundException(`Utilisateur ${player} introuvable`);
       }
     }
+  }
+
+  async join(user: User, joinLobbyDto: JoinLobbyDto) {
+    const lobby = await this.findLobbyByCode(joinLobbyDto.code);
+    if (lobby.players.includes(user.id)) {
+      throw new NotFoundException('Vous êtes déjà dans cette partie');
+    }
+    lobby.players.push(user);
+    await this.checkPlayersInLobby(lobby);
+    return (await (await lobby.save()).populate('owner')).populate('players');
+  }
+
+  async leave(user: User, id: string) {
+    const lobby = await this.findLobbyById(id);
+    if (!lobby.players.includes(user.id)) {
+      throw new NotFoundException('Vous n\'êtes pas dans cette partie');
+    }
+    lobby.players = lobby.players.filter((player) => String(player) !== user.id);
+    if (lobby.players.length === 0) {
+      await this.remove(lobby.id);
+      return;
+    }
+    if (user.id === lobby.owner) {
+      lobby.owner = lobby.players[0] || null;
+    }
+    await this.checkPlayersInLobby(lobby);
+    return (await (await lobby.save()).populate('owner')).populate('players');
+  }
+
+  async findLobbyByCode(code: string) {
+    const lobby = await this.lobbyModel.findOne({ code });
+    if (!lobby) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    return lobby;
+  }
+
+  async findLobbyById(id: string) {
+    const lobby = await this.lobbyModel.findById(id);
+    if (!lobby) {
+      throw new NotFoundException('Lobby introuvable');
+    }
+    return lobby;
   }
 }
